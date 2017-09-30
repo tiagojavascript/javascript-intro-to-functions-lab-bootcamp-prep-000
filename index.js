5c3f6a5593eb10179535b83c3f1ca328bb5d3fd8
Tiago Lopes <tiagojavascript@gmail.com>
2017-09-30 01:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string === "i love you, Grandma."){
-    return "I love you too";
+    return "I love you, too.";
   } else if (string.toUpperCase() === string){
     return "YES INDEED!";
   } else if (string.toLowerCase() === string){
