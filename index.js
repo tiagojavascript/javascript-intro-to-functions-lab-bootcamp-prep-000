08dda5b3e18c1c4284f334bf667f53a61d1904cc
Tiago Lopes <tiagojavascript@gmail.com>
2017-09-30 01:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 };
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === true){
+  if (string.toLowerCase()){
     return "I can't hear you!"
   } else if (string.toUpperCase()){
     return "YES INDEED!"
